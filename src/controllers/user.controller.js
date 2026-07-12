47ad20ac1b9cd62f145d3dec96b41215cc175c7b
Shrees Singh <sshridhanshi@gmail.com>
2026-07-12 14:43:56 +0530

    add getWatchHistory functionality to user controller and routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import { uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse} from "../utils/ApiResponse.js";
 import { json } from "express";
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens =async(userId) => 
     {
@@ -411,6 +412,61 @@ const getUserChannelProfile = asyncHandler(async(req, res)=> {
 
 })
 
+const getWatchHistory = asyncHandler(async(req , res) => {
+   const user =await User.aggregate([
+    {
+        $match:{
+            _id: new mongoose.Types.ObjectId(req.user._id)
+        }
+    },
+    {
+        $lookup:{
+            from :"videos",
+            localField:"WatchHistory",
+            foreignField:"_id",
+            as:"watchHistory",
+            pipeline:[
+                {
+                $lookup:{
+                    from :"users",
+                    localField:"owner",
+                    foreignField:"_id",
+                    as:"owner",
+                    pipeline:[
+                        {
+                            $project:{
+                                fullName:1,
+                                username:1,
+                                avatar:1
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                $addFields:{
+                    owner:{
+                        $first:"$owner"
+                    }
+                }
+            }
+        ]
+        }
+    }
+   ])
+
+   return res
+   .status(200)
+   .json(new ApiResponse(200
+    ,user[0].getWatchHistory,
+    "watch history fetched successfully "
+   ))
+})
+
+
+
+
+
 export {
     registerUser,
     loginUser,
@@ -421,5 +477,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 }
